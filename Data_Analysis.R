8994c46f0dcd6a1d8f065f23d11aeccb0c08d324
Karan Yadav <76391007+karanyadav14@users.noreply.github.com>
2022-05-01 22:51:43 +0530

    Add files via upload

---
+++ Data_Analysis.R
@@ -0,0 +1,29 @@
+# Research Methods labs
+
+library(dplyr)
+library(lme4)
+library(ggplot2)
+
+
+data <- read.csv("hindi_JEMR_new.csv")
+head(data)
+str(data)
+  summary(data)
+
+data$subj <- as.factor(data$subj)
+data$item <- as.factor(data$item)
+
+
+
+m <- lmer(TFT ~ syll_len+word_len+word_complex+IC+SC+Word_Similarity
+          +(1|subj),
+          data = data,
+
+)
+
+
+summary(m)
+
+
+
+
